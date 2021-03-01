559cf4dd94f05e8c4dd514f845b407e5bc6f5499
Arthur Mitsuo <nkc_arthur@hotmail.com>
2021-03-01 13:25:07 -0300

    adicionando o valor do IMC calculado, em variável, como conteúdo na página index.html, por javascript

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -9,6 +9,12 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+var tdImc = paciente.querySelector(".info-imc");
+
 var imc = peso / (altura * altura);
+
+
+
+tdImc.textContent = imc;
 
 console.log(imc);
