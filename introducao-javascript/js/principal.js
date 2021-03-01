0f4344475a514a315c491360d46eeb1e879cac98
Arthur Mitsuo <nkc_arthur@hotmail.com>
2021-03-01 14:17:24 -0300

    utilizando if e os operadores lógicos ou(||) e e(&&) para validar peso e altura para cálculo do IMC do #primeiroPaciente

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -11,10 +11,24 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 400){
+  console.log("Peso inválido");
+  pesoValido = false;
+  tdImc.textContent = "Peso inválido!";
+}
+if(altura <= 0 || altura >= 3.00){
+  console.log("Altura inválida");
+  alturaValida = false;
+  tdImc.textContent = "Altura inválida!";
+}
+if(alturaValida && pesoValido){
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+}
 
 
-
-tdImc.textContent = imc;
 
 console.log(imc);
