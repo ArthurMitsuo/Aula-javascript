aba8d2af8466cdee8e1deba8edd1f7a0f9eec34d
Arthur Mitsuo <nkc_arthur@hotmail.com>
2021-03-02 14:21:04 -0300

    separação de arquivos js por funções realizadas

---
+++ introducao-javascript/js/form.js
@@ -0,0 +1,39 @@
+//arquivo responsável para tudo que tem relação com o form
+//botão de adicionar paciente, na form
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+
+  var form = document.querySelector("#formAdiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr") ;
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+  // imcTd.textContent = imc.toFixed(2);
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+
+});
