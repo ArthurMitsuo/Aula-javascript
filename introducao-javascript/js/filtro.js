e274c7fcde4fde0f757fe4c002dbb10a1d9a4f79
Arthur Mitsuo <nkc_arthur@hotmail.com>
2021-03-04 19:22:17 -0300

    filtro melhorando, quando o input com length 0, todos os nomes aparecem, quando não, os nomes não iguais ao digitado são tranferidos para uma classe com display: none

--- introducao-javascript/js/filtro.js
+++ introducao-javascript/js/filtro.js
@@ -2,11 +2,26 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
   console.log(this.value);
-  var paciente = document.querySelectorAll(".paciente"); //retorna todos os pacientes em array
-  for(var i = 0; i < paciente.length; i++){
-    var paciente = pacientes[i];
-    var tdNome = paciente.querySelector(".info-nome"); //buscando dentro da paciente buscamos uma td com a classe info-nome
-    var nome = paciente.textContent;
+  var pacientes = document.querySelectorAll(".paciente"); //retorna todos os pacientes em array
+
+  if(this.value.length > 0){
+    // console.log("tem algo digitado");
+    for(var i = 0; i < pacientes.length; i++){
+      var paciente = pacientes[i];
+      var tdNome = paciente.querySelector(".info-nome"); //buscando dentro da paciente buscamos uma td com a classe info-nome
+      var nome = tdNome.textContent;
+
+      if(nome != this.value){               //se o nome for diferente do que está sendo digitado
+        paciente.classList.add("invisivel");
+      }else{
+        paciente.classList.remove("invisivel");
+      }
+    }
+  }else{
+    for (var i = 0; i < pacientes.length; i++){
+      var paciente = pacientes[i];
+      paciente.classList.remove("invisivel");
+    }
   }
 
 })
