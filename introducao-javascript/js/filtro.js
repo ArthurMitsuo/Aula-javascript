3c85f4ed50416e67ee9c404926d9ba7689b47ca1
Arthur Mitsuo <nkc_arthur@hotmail.com>
2021-03-05 17:31:00 -0300

    comentários adicionados, assim como a utilização da RegExp() que recebe o que gostaria de buscar e como gostaria

--- introducao-javascript/js/filtro.js
+++ introducao-javascript/js/filtro.js
@@ -10,14 +10,15 @@ campoFiltro.addEventListener("input", function(){
       var paciente = pacientes[i];
       var tdNome = paciente.querySelector(".info-nome"); //buscando dentro da paciente buscamos uma td com a classe info-nome
       var nome = tdNome.textContent;
+      var expressao = new RegExp(this.value, "i"); //expressao regular, recebe o valor do campoFiltro com case insensitive
 
-      if(nome != this.value){               //se o nome for diferente do que está sendo digitado
-        paciente.classList.add("invisivel");
+      if(!expressao.test(nome)){               //se o nome for diferente do que está sendo digitado
+        paciente.classList.add("invisivel"); //adiciona na classe do css de nome invisível
       }else{
-        paciente.classList.remove("invisivel");
+        paciente.classList.remove("invisivel"); //senão, tira a classe invisível
       }
     }
-  }else{
+  }else{                                                // tira a classe de invisível se nada foi digitado
     for (var i = 0; i < pacientes.length; i++){
       var paciente = pacientes[i];
       paciente.classList.remove("invisivel");
